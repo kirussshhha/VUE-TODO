0556ec1d6594de0f7053ddb14186ebf541536a32
kirussshhha <kirill2006may@gmail.com>
2024-10-24 22:29:53 +0300

    Initial layout with styles added

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,47 +1,63 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="wrapper">
+    <div class="todo__container">
+      <h2 class="todo-header">to do list</h2>
+      <div class="list__wrapper">
+        <div class="list__container">
+          <ul class="task-list">
+            <li class="task">
+              <div class="task-checkbox">
+                <input type="checkbox" id="task1" class="task-inp" />
+                <label for="task1" class="task-label">Task 1</label>
+              </div>
+              <div class="remove-task">
+                <button class="remove-task-btn">✕</button>
+              </div>
+            </li>
+            <li class="task">
+              <div class="task-checkbox">
+                <input type="checkbox" id="task2" class="task-inp" />
+                <label for="task2" class="task-label">Task 2</label>
+              </div>
+              <div class="remove-task">
+                <button class="remove-task-btn">✕</button>
+              </div>
+            </li>
+            <li class="task">
+              <div class="task-checkbox">
+                <input type="checkbox" id="task2" class="task-inp" />
+                <label for="task2" class="task-label">Task 2</label>
+              </div>
+              <div class="remove-task">
+                <button class="remove-task-btn">✕</button>
+              </div>
+            </li>
+          </ul>
+          <div class="add-task">
+            <button class="add-task-btn">Add a new task</button>
+          </div>
+        </div>
+      </div>
+      <div class="nav__wrapper">
+        <div class="nav__container">
+          <p class="items-left">1/3 left</p>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+          <div class="btn-nav__container">
+            <button class="all-nav-btn">All</button>
+            <button class="nav-btn">Active</button>
+            <button class="nav-btn">Completed</button>
+          </div>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <img class="list-img" src="./assets/img/list.png" alt="Список" />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+export default {}
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style lang="scss">
+@use './styles/main.scss';
 </style>
